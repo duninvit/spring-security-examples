d47ccac4a7fb9d8516b11b500f74885fb98fd342
a2.dunin <a2.dunin@itransition.com>
2021-05-23 02:06:03 +0300

    Init spring security jwt recourse server

---
+++ 1-spring-security-jwt-resource-server/src/test/java/com/github/duninvit/jwtresourceserver/JwtResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.duninvit.jwtresourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtResourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
