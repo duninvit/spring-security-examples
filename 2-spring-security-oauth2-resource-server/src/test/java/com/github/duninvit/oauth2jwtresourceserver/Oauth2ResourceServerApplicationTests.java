c8893c1b158ce330dd23e99a5aa3954288e07681
duninvit <dunin.alex.vit@gmail.com>
2021-08-28 00:33:03 +0300

    Added auth and resource servers for oauth2 spring security.

---
+++ 2-spring-security-oauth2-resource-server/src/test/java/com/github/duninvit/oauth2jwtresourceserver/Oauth2ResourceServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.duninvit.oauth2jwtresourceserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Oauth2ResourceServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
